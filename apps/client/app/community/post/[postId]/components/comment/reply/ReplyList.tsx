abb2d105297918eee977a72673adaf51b025a5ca
developersung13 <developersung13@gmail.com>
2024-09-09 16:16:43 +0900

    feat: 커뮤니티 게시글 정보 조회 API 연동

--- apps/client/app/community/post/[postId]/components/comment/reply/ReplyList.tsx
+++ apps/client/app/community/post/[postId]/components/comment/reply/ReplyList.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { ReplyInfo } from '@/types/reply';
-import ReplyListItem from './ReplyListItem';
+import React from "react";
+import { ReplyInfo } from "@/types/reply";
+import ReplyListItem from "./ReplyListItem";
+import { commentInfo } from "@/data/mock/commentInfos";
 
 interface ReplyListProps {
+  postId: string;
+  commentId: number;
   replyInfos: ReplyInfo[];
   setIsOpenCommentAndReplyManagingBottomDrawer: React.Dispatch<
     React.SetStateAction<boolean>
@@ -10,13 +13,20 @@ interface ReplyListProps {
 }
 
 export default function ReplyList(props: ReplyListProps) {
-  const { replyInfos, setIsOpenCommentAndReplyManagingBottomDrawer } = props;
+  const {
+    postId,
+    commentId,
+    replyInfos,
+    setIsOpenCommentAndReplyManagingBottomDrawer,
+  } = props;
 
   return (
-    <div className='mt-1 flex flex-col gap-y-3'>
+    <div className="mt-1 flex flex-col gap-y-3">
       {replyInfos?.length !== 0 &&
         replyInfos?.map((replyInfo: ReplyInfo, index: number) => (
           <ReplyListItem
+            postId={postId}
+            commentId={commentId}
             replyInfo={replyInfo}
             key={index}
             setIsOpenCommentAndReplyManagingBottomDrawer={
