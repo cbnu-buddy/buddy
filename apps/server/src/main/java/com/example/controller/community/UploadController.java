70ba76313d0d5980f2d9e51ba612920930660fcb
developersung13 <developersung13@gmail.com>
2024-09-30 15:41:52 +0900

    fix: 파일 업로드 API 엔드포인트 정정

--- apps/server/src/main/java/com/example/controller/community/UploadController.java
+++ apps/server/src/main/java/com/example/controller/community/UploadController.java
@@ -16,7 +16,6 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/private/upload")
 public class UploadController {
   private final UploadService uploadService;
 
